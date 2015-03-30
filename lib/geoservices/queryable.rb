455b1f2cc314cf28a2a34bfab02d405f72a00904
dmfenton <dfenton@esri.com>
2015-03-30 16:10:57 -0400

    adds test and support for getting count for old services

--- lib/geoservices/queryable.rb
+++ lib/geoservices/queryable.rb
@@ -14,7 +14,15 @@ module Geoservice
     # returns nil if the layer does not have the Capability: Query
     def count(layer_idx=0, options={})
       return nil unless self.metadata["capabilities"] =~ /Query/
-      query(layer_idx, options.merge(:returnCountOnly => true))
+      # Current Version is not returned at lower than 10.1
+      unless self.metadata['currentVersion'].nil?
+        query(layer_idx, options.merge(:returnCountOnly => true))
+      else
+        # Return count only is not supported by versions lower than 10.1
+        # So request all the object ids and count them up
+        response = query(layer_idx, options.merge(:where => '1=1', :returnIdsOnly => true))
+        return {'count' => response['objectIds'].length}
+      end
     end
   end
 end
